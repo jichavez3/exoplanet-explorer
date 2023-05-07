eebf450889d7b7b06f2a4e4b1f165a4ad6333dae
Jason <jason.chavez.3@gmail.com>
2023-05-07 16:36:53 -0400

    completed quiz

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -64,9 +64,15 @@ Hint: you'll probably still need to use .map.
 
       addSearchHeader(response.query);
 
-      response.results.map(function(url) {
-        getJSON(url).then(createPlanetThumb);
-      });
+      Promise.all(response.results.map(getJSON))
+      .then(function(planetData){
+        planetData.forEach(function(planet){
+          createPlanetThumb(planet);
+        })
+      })
+      .catch(function(error){
+        console.log(error);
+      })
     });
   });
 })(document);
